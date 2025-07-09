31d77a7987c253577eb17c943e3aa409a583d77c
njatoty <njatotianafiononana@gmail.com>
2025-07-09 17:52:26 +0300

    feat: clear drawing when drawingEnabled is disabled and layer canvas is available

--- src/hooks/use-canvas-snap.ts
+++ src/hooks/use-canvas-snap.ts
@@ -1,13 +1,11 @@
-
-import { useEffect, useMemo, useRef } from 'react';
-import type { CanvasSnapOptions, SnapshotProps } from '../types';
-import { mergeSnapOptions } from '../lib/utils';
-import { useCanvasLayer } from './use-canvas-layer';
-import { useRectangleDrawing } from './use-rectangle-drawing';
-import { useMouseEvents } from './use-mouse-events';
-import { useImageCapture } from './use-image-capture';
-import { useKeyboardEvents } from './use-keyboard-events';
-
+import React, { useEffect, useMemo, useRef } from "react";
+import type { CanvasSnapOptions, SnapshotProps } from "../types";
+import { mergeSnapOptions } from "../lib/utils";
+import { useCanvasLayer } from "./use-canvas-layer";
+import { useRectangleDrawing } from "./use-rectangle-drawing";
+import { useMouseEvents } from "./use-mouse-events";
+import { useImageCapture } from "./use-image-capture";
+import { useKeyboardEvents } from "./use-keyboard-events";
 
 const DEFAULT_OPTIONS: CanvasSnapOptions = {
     drawingEnabled: false,
@@ -15,7 +13,7 @@ const DEFAULT_OPTIONS: CanvasSnapOptions = {
         borderStyle: "dashed",
         borderColor: "#F14236",
         borderWidth: 1,
-        outterBackgroundColor: "rgba(0, 0, 0, 0.1)"
+        outterBackgroundColor: "rgba(0, 0, 0, 0.1)",
     },
     copyImageToClipBoard: true,
     imageQuality: "high",
@@ -24,41 +22,52 @@ const DEFAULT_OPTIONS: CanvasSnapOptions = {
     helperText: {
         show: true,
         value: "Press Enter to capture, Escape to cancel",
-        position: "bottom-center",
+        position: "bottom-right",
         style: {
             backgroundColor: "#F14236",
             textColor: "#fff",
             fontSize: 12,
-            fontFamily: "Arial",
+            fontFamily: "Calibri, sans-serif",
             padding: 4,
-            textHeight: 12
-        }
-    }
+            textHeight: 12,
+        },
+    },
 };
 
 export function useCanvasSnap(
     ref: React.RefObject<HTMLCanvasElement>,
     callBack?: (snapshot: SnapshotProps) => void,
-    options?: CanvasSnapOptions,
+    options?: CanvasSnapOptions
 ) {
-
     const canvasRef = ref ?? useRef<HTMLCanvasElement>(null);
     const canvas = canvasRef.current;
 
     // merge options to get config
-    const defaultOption = useMemo(() => (options === undefined) ? DEFAULT_OPTIONS : mergeSnapOptions(DEFAULT_OPTIONS, options), [options]);
-
+    const defaultOption = useMemo(
+        () =>
+            options === undefined
+                ? DEFAULT_OPTIONS
+                : mergeSnapOptions(DEFAULT_OPTIONS, options),
+        [options]
+    );
 
     // Create canvas layer
-    const { layerCanvas } = useCanvasLayer(canvas, defaultOption.drawingEnabled, defaultOption);
+    const { layerCanvas } = useCanvasLayer(
+        canvas,
+        defaultOption.drawingEnabled,
+        defaultOption
+    );
 
     // Rectangle drawing logic
     const { isDrawing, setIsDrawing, rectCoords, setRectCoords, clearDrawing } =
         useRectangleDrawing(layerCanvas, defaultOption);
 
     // Image capture logic
-    const { captureRectAsImage, copyImageToClipboard } =
-        useImageCapture(canvas, rectCoords, defaultOption);
+    const { captureRectAsImage, copyImageToClipboard } = useImageCapture(
+        canvas,
+        rectCoords,
+        defaultOption
+    );
 
     // Mouse event handling
     useMouseEvents(layerCanvas, isDrawing, setRectCoords, setIsDrawing);
@@ -77,6 +86,14 @@ export function useCanvasSnap(
         captureRectAsImage,
         clearDrawing
     );
+
+    // Effect to clear drawing when drawingEnabled is disabled and layerCanvas is available
+    useEffect(() => {
+        if (!defaultOption.drawingEnabled && layerCanvas) {
+            clearDrawing();
+        }
+    }, [defaultOption.drawingEnabled, clearDrawing, layerCanvas]);
+
 
     return { canvasRef };
 }
