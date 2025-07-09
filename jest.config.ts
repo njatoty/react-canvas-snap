9366b198706d74c0f0ab82b8ed2e232ef99e005e
njatoty <njatotianafiononana@gmail.com>
2025-07-09 20:03:40 +0300

    feat: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testMatch: ['**/__tests__/**/*.test.tsx'],
+};
+
+export default config;
