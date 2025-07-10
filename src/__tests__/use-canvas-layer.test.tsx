bfd24dc850ed7e1f0d77384a242b4090cf9e7412
njatoty <njatotianafiononana@gmail.com>
2025-07-10 15:04:14 +0300

    add: global options

--- src/__tests__/use-canvas-layer.test.tsx
+++ src/__tests__/use-canvas-layer.test.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import TestComponent from "./components/TestComponent";
 import type { CanvasSnapOptions } from "../types";
 
 describe("useCanvasLayer", () => {
-  it("adds a cloned canvas layer when drawing is enabled", async () => {
-    const options: CanvasSnapOptions = {
-      cursor: "crosshair",
-      rect: { outterBackgroundColor: "rgba(0,0,0,0.5)" },
-      drawingEnabled: true,
-    };
+  const options: CanvasSnapOptions = {
+    cursor: "crosshair",
+    rect: { outterBackgroundColor: "rgba(0,0,0,0.5)" },
+    drawingEnabled: true,
+  };
 
-    const { container } = render(
+  it("adds a cloned canvas layer when drawing is enabled", async () => {
+    render(
       <TestComponent drawingEnabled={true} options={options} />
     );
-    const mainCanvas = container.querySelector(
-      'canvas[data-testid="main-canvas"]'
-    );
-    const layerCanvas = container.querySelector("canvas.rcs__drawer-layer");
 
+    const mainCanvas = screen.getByTestId("main-canvas");
     expect(mainCanvas).toBeInTheDocument();
+
+    const layerCanvas = screen.getByRole('img', { name: 'Canvas Layer' });
     expect(layerCanvas).toBeInTheDocument();
+
     expect(layerCanvas).toHaveStyle({
       position: "absolute",
       cursor: "crosshair",
@@ -28,15 +28,10 @@ describe("useCanvasLayer", () => {
   });
 
   it("does not add a layer if drawingEnabled is false", () => {
-    const options: CanvasSnapOptions = {
-      cursor: "crosshair",
-      rect: { outterBackgroundColor: "rgba(0,0,0,0.5)" },
-      drawingEnabled: true,
-    };
-
     const { container } = render(
       <TestComponent drawingEnabled={false} options={options} />
     );
+
     const layerCanvas = container.querySelector("canvas.rcs__drawer-layer");
 
     expect(layerCanvas).not.toBeInTheDocument();
@@ -59,6 +54,6 @@ describe("useCanvasLayer", () => {
     rerender(<TestComponent drawingEnabled={true} options={options} />);
     expect(container.querySelectorAll("canvas.rcs__drawer-layer")).toHaveLength(
       1
-    ); // Still only one layer
+    );
   });
 });
