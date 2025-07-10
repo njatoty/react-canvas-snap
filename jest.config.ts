a8602e888a1be2eb882b2ba4c829a63ef7ab454a
njatoty <njatotianafiononana@gmail.com>
2025-07-10 08:27:56 +0300

    chore: install and configure jest-canvas-mock

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts', 'jest-canvas-mock'],
     testMatch: ['**/__tests__/**/*.test.tsx'],
 };
 
