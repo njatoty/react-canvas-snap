b56392dffa03d4474a7ca883839c3b8afd037cc8
njatoty <njatotianafiononana@gmail.com>
2025-07-11 10:54:47 +0300

    add: method to draw measurment labels

---
+++ src/hooks/use-rect-labels.ts
@@ -0,0 +1,98 @@
+import { useCallback } from 'react';
+import { RectCoords, HelperTextConfig, Position, MeasurementLabelConfig } from '../types';
+import { calculateAutoPosition, drawMeasurementLabel } from '../lib/draw';
+
+export const useRectLabels = () => {
+    const drawHelperText = useCallback((
+        ctx: CanvasRenderingContext2D,
+        rectCoords: RectCoords,
+        canvasWidth: number,
+        canvasHeight: number,
+        helperText: HelperTextConfig
+    ) => {
+        if (!helperText.show) return;
+
+
+        const { x: X, y: Y, width, height } = rectCoords;
+        if (width === 0 || height === 0) return;
+
+        const fontSize = helperText.style?.fontSize || 10;
+        const fontFamily = helperText.style?.fontFamily || 'Arial';
+        const padding = helperText.style?.padding || 2;
+        const backgroundColor = helperText.style?.backgroundColor || '#000';
+        const textColor = helperText.style?.textColor || '#fff';
+
+        // Determine dynamic helper text position
+        let position: Position = helperText.position || 'auto';
+        if (position === 'auto') {
+            const textWidth = ctx.measureText(helperText.value || '').width;
+            const marginY = fontSize * 2 + padding;
+            position = calculateAutoPosition(
+                X, Y, width, height,
+                textWidth, marginY,
+                canvasWidth, canvasHeight
+            );
+        }
+
+        drawMeasurementLabel({
+            ctx,
+            text: helperText.value || '',
+            position,
+            fontSize,
+            fontFamily,
+            backgroundColor,
+            textColor,
+            rectCoords,
+            padding
+        });
+    }, []);
+
+    const drawMeasurementLabels = useCallback((
+        ctx: CanvasRenderingContext2D,
+        rectCoords: RectCoords,
+        config: MeasurementLabelConfig
+    ) => {
+        if (!config.show) return;
+
+        const fontSize = config.style?.fontSize!;
+        const fontFamily = config.style?.fontFamily!;
+        const padding = config.style?.padding!;
+        const backgroundColor = config.style?.backgroundColor!;
+
+        const { width, height } = rectCoords;
+
+        if (config.showWidth) {
+            drawMeasurementLabel({
+                ctx,
+                text: `${width}px`,
+                position: config.positionWidth!,
+                fontSize,
+                fontFamily,
+                backgroundColor: 'transparent',
+                textColor: backgroundColor,
+                rectCoords,
+                padding
+            });
+        }
+
+        if (config.showHeight) {
+            drawMeasurementLabel({
+                ctx,
+                text: `${height}px`,
+                position: config.positionHeight!,
+                fontSize,
+                fontFamily,
+                backgroundColor: 'transparent',
+                textColor: backgroundColor,
+                rectCoords,
+                padding
+            });
+        }
+    }, []);
+
+    return {
+        drawHelperText,
+        drawMeasurementLabels
+    };
+};
+
